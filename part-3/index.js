f84a055952f416c53160bc000a6d50c28bed3c1a
Matthew <matthewkimcs@gmail.com>
2023-03-26 14:10:52 -0700

    studied more about node.js and express

--- part-3/index.js
+++ part-3/index.js
@@ -1,7 +1,23 @@
 const express = require("express");
 const app = express();
 
-let notes = ["bing chilling"];
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
+];
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
@@ -9,6 +25,30 @@ app.get("/", (request, response) => {
 
 app.get("/api/notes", (request, response) => {
   response.json(notes);
+});
+
+app.get("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find((note) => note.id === id);
+  /* we need the if statement to send the response with a status code of 404 if there is 
+  no note because normally the server will respond with status code 200 even though note is undefined
+  */
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
+  response.json([123]);
+  response.json(note);
+});
+
+app.delete("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
+  /* 204 status code indicates that the server successfully fulfilled the request
+  and that there is no additional content to send in the response content
+  */
+  response.status(204).end();
 });
 
 const PORT = 3001;
