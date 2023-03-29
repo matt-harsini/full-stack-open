17eeb00b4bc7b3173c82a995bee9d0d6091ebcfd
Matthew <matthewkimcs@gmail.com>
2023-03-29 10:45:19 -0700

    working on connecting frontend to backend

--- part-3a-exs/index.js
+++ part-3a-exs/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 app.use(express.json());
+app.use(cors);
 
 morgan.token("body", (request, response) => {
   return JSON.stringify(request.body);
